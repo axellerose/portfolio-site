e4326692bcf25ca3a9d6f672ba2a8acac4249e4d
alex <iamalex.dev@gmail.com>
2021-04-21 15:45:00 +0200

    <feat> : integrate EmailJS for sending messages

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Box from './common/Box';
 import Avatar from '../assets/avatar.png';
+import emailjs from 'emailjs-com';
 
 const Container = styled(Box)`
     a {
@@ -68,6 +69,44 @@ const StyledForm = styled.form`
 `
 
 const Contacts = () => {
+
+    const [template, setTemplate] = useState({
+        user_name: '',
+        user_email: '',
+        subject: '',
+        message: '',
+    });
+
+    const sendEmail = e => {
+        const SERVICE_ID = "service_mbnbpk5";
+        const TEMPLATE_ID = "template_f0p5w5h";
+        const USER_ID = "user_jZrXi2I6JqNGUNyQZzy8N"
+
+        e.preventDefault();
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+            .then((result) => {
+                console.log(result.text);
+                alert("Thanks for reaching me out ! I will contact you back soon, cheers!");
+                clearForm();
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
+    const changeHandler = e => {
+        setTemplate({ ...template, [e.target.name]: e.target.value })
+    }
+
+    const clearForm = () => {
+        setTemplate({
+            user_name: '',
+            user_email: '',
+            subject: '',
+            message: '',
+        })
+    }
+
     return (
         <Container display="flex" flexDirection="column">
             <StyledDescription>
@@ -85,19 +124,51 @@ const Contacts = () => {
                 </Box>
             </Box>
 
-            <StyledForm id="contacts">
+            <StyledForm id="contacts" className="contact-form" onSubmit={sendEmail}>
                 <h3 >Send me a message</h3>
                 <Box className="form__control" >
                     <label htmlFor="">Name :</label>
-                    <input type="text" placeholder="Richard Hendricks" />
+                    <input
+                        type="text"
+                        name="user_name"
+                        value={template.user_name}
+                        placeholder="Richard Hendricks"
+                        onChange={changeHandler}
+                        required
+                    />
                 </Box>
                 <Box className="form__control">
                     <label htmlFor="">Email :</label>
-                    <input type="email" placeholder="richard@piedpiper.com" />
+                    <input
+                        type="email"
+                        name="user_email"
+                        value={template.user_email}
+                        placeholder="richard@piedpiper.com"
+                        onChange={changeHandler}
+                        required
+                    />
                 </Box>
                 <Box className="form__control">
-                    <label htmlFor="">Message :</label>
-                    <textarea type="text" rows="3" placeholder="Hi there! I've got a job for you!" />
+                    <label htmlFor="">Subject :</label>
+                    <input
+                        type="text"
+                        name="subject"
+                        value={template.subject}
+                        placeholder="optional"
+                        onChange={changeHandler}
+                    />
+                </Box>
+                <Box className="form__control">
+                    <label htmlFor="message">Message :</label>
+                    <textarea
+                        required
+                        name="message"
+                        value={template.message}
+                        type="text"
+                        rows="3"
+                        placeholder="Hi there! I've got a job for you!"
+                        onChange={changeHandler}
+                    />
                 </Box>
                 <button>Send</button>
             </StyledForm>
